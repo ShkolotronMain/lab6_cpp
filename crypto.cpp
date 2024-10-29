6f9e722c442cde86557129c6e62355eccec0db81
ShkolotronMain <susbus5002@gmail.com>
2024-10-29 18:35:17 +0300

    base child

---
+++ crypto.cpp
@@ -0,0 +1,88 @@
+#include "crypto.hpp"
+
+Crypto::Crypto()
+{
+    currency = "";
+    state = "";
+    code = "";
+    subunit = "";
+    fraction = 0;
+    rate = 0;
+    system = "";
+    language = "";
+    year = 0;
+}
+
+Crypto::Crypto(json val)
+{
+    currency = val["currency"];
+    state = val["state"];
+    code = val["code"];
+    subunit = val["subunit"];
+    fraction = val["fraction"];
+    rate = val["rate"];
+    system = val["system"];
+    language = val["language"];
+    rate = val["rate"];
+}
+
+json Crypto::get_object()
+{
+    json value;
+    value["type"] = "crypto";
+    value["currency"] = currency;
+    value["state"] = state;
+    value["code"] = code;
+    value["subunit"] = subunit;
+    value["fraction"] = fraction;
+    value["rate"] = rate;
+    value["system"] = system;
+    value["language"] = language;
+    value["year"] = year;
+    return value;
+}
+
+ostream& operator<<(ostream& out, const Crypto& cl)
+{
+    out << "===================" << endl;
+    out.setf(ios::fixed);
+    out.precision(2);
+    out << cl.currency << endl;
+    out << cl.state << endl;
+    out << cl.code << endl;
+    out << "1 " + cl.code + " = " << cl.rate << " RUB" << endl;
+    out << cl.system << " system" << endl;
+    out << "Started in " << cl.year << endl;
+    out << "Written in " << cl.language << endl;
+    out << "===================" << endl;
+
+    return out;
+}
+
+istream &operator>>(istream& in, Crypto& c)
+{
+    string trash;
+
+    getline(in, trash);
+    cout << "Введите название валюты:" << endl;
+    getline(in, c.currency);
+    cout << "Введите государство:" << endl;
+    getline(in, c.state);
+    cout << "Введите код валюты:" << endl;
+    getline(in, c.code);
+    cout << "Введите название разменной валюты:" << endl;
+    getline(in, c.subunit);
+    cout << "Сколько разменной валюты в основной:" << endl;
+    in >> c.fraction;
+    cout << "Введите курс валюты к российскому рублю:" << endl;
+    in >> c.rate;
+    cout << "Введите экосистему криптовалют" << endl;
+    getline(in, c.system);
+    cout << "Введите язык реализации криптовалюты" << endl;
+    getline(in, c.language);
+    cout << "Введите год запуска криптовалюты" << endl;
+    in >> c.year;
+    getline(in, trash);
+
+    return in;
+}
---
+++ crypto.hpp
@@ -0,0 +1,24 @@
+#include "course.hpp"
+
+// Класс-наследник
+class Crypto : Course
+{
+    private:
+        // система криптовалют
+        string system;
+        // язык, на котором написана обработка
+        string language;
+        // год запуска
+        int year;
+
+    public:
+        Crypto();
+        Crypto(json);
+
+        json get_object();
+
+        // Консольный вывод
+        friend ostream &operator<<(ostream&, const Crypto&);
+        // Консольный ввод
+        friend istream &operator>>(istream&, Crypto&);
+};
